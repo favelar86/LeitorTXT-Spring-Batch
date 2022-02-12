758767ce25f478e620d0b6226c63f5efe1ffa45c
Fabiano Avelar <fabiano_fernandesavelar@yahoo.com.br>
2022-02-11 21:52:55 -0300

    Projeto criado

---
+++ src/main/java/br/com/springBatch/sequencialToBD/SequencialToBdApplication.java
@@ -0,0 +1,13 @@
+package br.com.springBatch.sequencialToBD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SequencialToBdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SequencialToBdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/springBatch/SequencialToBD/SequencialToBdApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.springBatch.SequencialToBD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SequencialToBdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
